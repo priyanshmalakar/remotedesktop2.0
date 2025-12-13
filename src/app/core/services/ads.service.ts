bb8b075eec675c5895c3a3b00ae7fb876915e7a5
rupen-01 <rupensingj7@gmail.com>
2025-12-13 12:42:12 +0530

    live link add backend

--- src/app/core/services/ads.service.ts
+++ src/app/core/services/ads.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AdsService {
 
-  private api = 'http://localhost:5000/api/ads';
+  private api = 'https://remote-desktop-landingpage-backend.onrender.com/api/ads';
 
   constructor(private http: HttpClient) {}
 
